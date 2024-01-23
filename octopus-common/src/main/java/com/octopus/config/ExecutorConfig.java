02a658fd0f2cf79ca7b607f60751213090428a6b
lufudi <1093626996@qq.com>
2024-01-23 10:59:52 +0800

    优化线程池的拒绝策略

--- octopus-common/src/main/java/com/octopus/config/ExecutorConfig.java
+++ octopus-common/src/main/java/com/octopus/config/ExecutorConfig.java
@@ -1,6 +1,10 @@
 package com.octopus.config;
 
+import cn.hutool.core.thread.BlockPolicy;
+import cn.hutool.core.thread.RejectPolicy;
 import cn.hutool.core.thread.ThreadFactoryBuilder;
+import cn.hutool.core.thread.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +13,7 @@ import java.util.concurrent.*;
 /**
  * @author Administrator
  */
+@Slf4j
 @Configuration
 public class ExecutorConfig {
 
@@ -23,7 +28,17 @@ public class ExecutorConfig {
     public ExecutorService excelExecutor() {
         ThreadFactory threadFactory = new ThreadFactoryBuilder().setDaemon(true)
                 .setNamePrefix(EXCEL_EXECUTOR.concat("_")).build();
-        return new ThreadPoolExecutor(5, 20,
-                5, TimeUnit.SECONDS, new ArrayBlockingQueue(2000), threadFactory);
+
+        return new ThreadPoolExecutor(4, 16,
+                5, TimeUnit.SECONDS, new ArrayBlockingQueue(200), threadFactory, (r, executor) -> {
+                    try {
+                        log.info("Thread Pool is full");
+                        //如果队列已满，则阻塞线程，直到有空间可用。确保任务一定执行
+                        //批量任务可如此设置。若线程池的作用是并行加载资源(如页面数据)，则应采用直接拒绝的方式，否则会导致响应时间过长
+                        executor.getQueue().put(r);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
